a4565196173292b6bb7c43783f6cdf93949ae1a7
Pavel Deshchenia <drednout63@gmail.com>
2024-06-20 23:34:38 +0300

    add modal and JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 const button = document.getElementById("button");
+const buttonClose = document.getElementById("buttonCloseId");
+const modalWrapper = document.getElementById("modalWrapperId");
+
 button.addEventListener("click", () => {
-  console.log("click");
+  modalWrapper.style.display === "none"
+    ? (modalWrapper.style.display = "flex")
+    : (modalWrapper.style.display = "none");
+});
+
+buttonClose.addEventListener("click", () => {
+  modalWrapper.style.display === "flex"
+    ? (modalWrapper.style.display = "none")
+    : (modalWrapper.style.display = "flex");
 });
